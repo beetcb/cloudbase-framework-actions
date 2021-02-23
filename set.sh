f76695ef449bc5750db73e0a539355fbe0acd89b
be beet <63141491+beetcb@users.noreply.github.com>
2021-02-23 21:25:27 +0800

    feat: format and traces commands

--- set.sh
+++ set.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# Print commands
+# Trace commands
 set -v
 
-# Download and install cloudbase cli
+####### Download and install cloudbase cli ######
 sudo npm i -g @cloudbase/cli --loglevel=error
 
-# Login in
+####### Login in ################################
 tcb login --apiKeyId "$1" --apiKey "$2"
 
-# Deploy to cloudbase
+####### Deploy to cloudbase #####################
 tcb framework deploy -e "$3"
