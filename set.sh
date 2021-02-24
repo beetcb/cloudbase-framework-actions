c9c9accd1f80c7d9e57844388a31f7e111d6b9f2
be beet <63141491+beetcb@users.noreply.github.com>
2021-02-24 11:31:55 +0800

    test: npx run directly

--- set.sh
+++ set.sh
@@ -7,7 +7,7 @@ ENV="$3"
 # Trace commands
 trace() {
     set -x
-    eval $1
+    eval "$1"
     set +x
 }
 
@@ -15,7 +15,9 @@ echo -e "\e[36m####### Download and install cloudbase cli ######"
 trace "sudo npm i -g @cloudbase/cli --loglevel=error"
 
 echo -e "\e[36m####### Login in ################################"
-trace "tcb login --apiKeyId "ID" --apiKey "KEY""
+trace "tcb login --apiKeyId "$ID" --apiKey "$KEY""
 
 echo -e "\e[34m####### Deploy to cloudbase #####################"
-trace "tcb framework deploy -e "ENV""
+trace "tcb framework deploy -e "$ENV""
+
+
