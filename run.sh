3b6cb72c6b66fc90c72c97215523384e2792be89
be beet <63141491+beetcb@users.noreply.github.com>
2021-02-24 13:14:41 +0800

    feat: group core commands

--- run.sh
+++ run.sh
@@ -6,15 +6,14 @@ ENV="$3"
 
 # Trace commands
 trace() {
+    echo -e "::group::$2"
     echo -e "\e[32m› $1"
     eval "$1"
+    echo "::endgroup::"
 }
 
-echo -e "\e[36m####### Download and install cloudbase cli ######"
-trace "sudo npm i -g @cloudbase/cli --loglevel=error | grep "@cloudbase/cli""
+trace "sudo npm i -g @cloudbase/cli --loglevel=error | grep "@cloudbase/cli"" "\e[36m####### Download and install cloudbase cli ######"
 
-echo -e "\e[36m####### Login in ################################"
-trace "tcb login --apiKeyId "$ID" --apiKey "$KEY" | grep "登录""
+trace "tcb login --apiKeyId "$ID" --apiKey "$KEY" | grep "登录"" "\e[36m####### Login in ################################"
 
-echo -e "\e[34m####### Deploy to cloudbase #####################"
-trace "tcb framework deploy -e "$ENV""
+trace "tcb framework deploy -e "$ENV"" "\e[34m####### Deploy to cloudbase #####################"
